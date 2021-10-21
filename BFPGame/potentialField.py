da7330fb07308e64d727c899f40b61d24c885287
yuanhenglee <yuanhenglee2000@gmail.com>
2021-10-21 10:39:21 +0800

    add toggle bitmap feature

--- BFPGame/potentialField.py
+++ BFPGame/potentialField.py
@@ -1,29 +1,26 @@
 import utils
-import numpy as np
+import pygame
 
 
 class PotentialField:
     def __init__(self):
-        self.bitmap = [[0 for x in range(128)] for y in range(128)]
+        self.bitmap = [[254 for x in range(128)] for y in range(128)]
 
     def mark_obstacles(self, obstacles):
         for obstacle in obstacles:
             for c in obstacle.convex:
                 # mark where obstacle are
                 for point in utils.convex_boundaries(c):
-                    self.bitmap[point[0]][point[1]] = 255
+                    self.bitmap[point[0]][point[1]] = 0 # tmp
 
-    # def show_bitmap(self):
-        # np_bitmap = np.array(self.bitmap)
-        # fig = plt.figure()
-        # axe = fig.add_subplot()
+    def show_bitmap(self, gameDisplay):
+        for i in range(128):
+            for j in range(128):
+                color = self.bitmap[i][j]
+                color = (color, color, color)
+                x, y = utils.world2Canvas((i, j))
+                pygame.draw.rect(gameDisplay, color, [x, y, utils.multiplier, utils.multiplier], 1)
 
-        # # 生成隨機資料
-        # data = np.random.randn(100).cumsum()
-
-        # # 以 seaborn 呈現
-        # sns.heatmap(data=np_bitmap)
-        # plt.show()
-
-
-obstacles_bitmap = PotentialField()
+    def mark_NF1(self):
+        ...
+        
