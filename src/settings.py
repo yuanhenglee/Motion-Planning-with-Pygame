5d3d6daa600460c02bd4732c2573b79dc98482f9
yuanhenglee <1990henrylee@gmail.com>
2022-01-20 10:08:32 +0800

    refactor: settings reorder

--- src/settings.py
+++ src/settings.py
@@ -1,16 +1,17 @@
 import numpy as np
-import utils
 
+# state
 mode = 'drag'
 show_bitmap = False
-pf = None
-
-path_showing_step = 2
 show_path = False
+show_animation = False
+
+# global variables
+pf = None
 path = None
-
 obstacles_bitmap = np.full((128, 128), 254)
-
-
 robot_dat_path = "Dat/robot1.dat"
 obstacle_dat_path = "Dat/obstacle1.dat"
+
+# config
+path_showing_step = 2
